4f2d359781a4e5b4d17d808bafa8a583a593b909
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-04-27 19:02:25 -0300

    javascript
    code added to animate

---
+++ script.js
@@ -0,0 +1,7 @@
+function displayAbout(){
+    var arrow = document.getElementById('animation');
+    arrow.classList.toggle('rotate')
+
+    var display = document.getElementById('about');
+    arrow.classList.toggle('active')
+}
